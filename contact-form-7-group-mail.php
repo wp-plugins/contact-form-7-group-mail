e30a01f988e95d03b752b367c11008bf78c31883
gutob <gutob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-02 00:23:56 +0000

    adding version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-group-mail/trunk@620102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contact-form-7-group-mail.php
+++ contact-form-7-group-mail.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Contact Form 7 Group Mail
-Plugin URI: http://www.u3b.com.br/contact-form-7-group-mail
-Description: Send 'Contact Form 7' mails to all users of any group (superadmins, admins, editors, authors, contributors, subscribers).
-Version: 0.9
+Plugin URI: http://www.u3b.com.br/plugins/contact-form-7-group-mail
+Description: Send 'Contact Form 7' mails to all users of any group (admins, editors, authors, contributors, subscribers and customs roles).
+Version: 1.0
 Author: Augusto Bennemann
 Author URI: http://www.u3b.com.br
 License: GPL2
@@ -11,7 +11,7 @@ License: GPL2
 
 /*  Copyright 2012 Augusto Bennemann (email: gutobenn at gmail.com)
 
-	This program is free software; you can redistribute it and/or modify
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
@@ -34,13 +34,11 @@ function wpcf7_group_mail_create_menu() {
 }
 
 function wpcf7_group_mail_register_settings() {
-	 register_setting( 'wpcf7_group_mail-settings-group', 'mode' );
-    register_setting( 'wpcf7_group_mail-settings-group', 'superadmin' );
-    register_setting( 'wpcf7_group_mail-settings-group', 'administrator' );
-    register_setting( 'wpcf7_group_mail-settings-group', 'editor' );
-    register_setting( 'wpcf7_group_mail-settings-group', 'author' );
-    register_setting( 'wpcf7_group_mail-settings-group', 'contributor' );
-    register_setting( 'wpcf7_group_mail-settings-group', 'subscriber' );
+    register_setting( 'wpcf7_group_mail-settings-group', 'mode' );
+    $roles_keys = array_keys(get_editable_roles());
+    for($i = 0; $i < sizeof($roles_keys); $i++){
+        register_setting( 'wpcf7_group_mail-settings-group', $roles_keys[$i] );
+    }
 } 
 
 function wpcf7_group_mail_settings_page() {
@@ -50,26 +48,31 @@ function wpcf7_group_mail_settings_page() {
 
     Select groups to receive 'Contact Form 7' messages
     <form method="post" action="options.php">
-			<?php settings_fields( 'wpcf7_group_mail-settings-group' ); ?>
-			<table class="form-table">
 
-			<?php $nomes = array("superadmin", "administrator", "editor", "author", "contributor", "subscriber");
-				?>
-				<tr valign="top">
-				<th scope="row">Mode</th>
-				<td>
-					<select name="mode">
-						<option value="normal" <?php if(get_option("mode") == "normal")echo 'selected="selected"'; ?>>Normal</option>
-						<option value="cc" <?php if(get_option("mode") == "cc")echo 'selected="selected"'; ?> >Cc</option>
-						<option value="cco" <?php if(get_option("mode") == "cco")echo 'selected="selected"'; ?> >Cco</option>
-					</select>
-				</td></tr>
-				<?php for($i = 0; $i <= 5; $i++){?>
-					<tr valign="top">
-            	<th scope="row"><?=$nomes[$i]?></th>
-            	<td><input type="checkbox" name="<?=$nomes[$i]?>" <?php if (get_option($nomes[$i]) == "on") echo 'checked="checked"'; ?>/></td>
-            	</tr>
-				<?php }	?>
+        <?php settings_fields( 'wpcf7_group_mail-settings-group' ); ?>
+        <table class="form-table">
+
+            <?php 
+            $nomes = get_editable_roles();
+            $nomes_keys = array_keys($nomes);
+            ?>
+            <tr valign="top">
+                <th scope="row">Mode</th>
+                <td>
+                    <select name="mode">
+                        <option value="normal" <?php if(get_option("mode") == "normal")echo 'selected="selected"'; ?>>Normal</option>
+                        <option value="cc" <?php if(get_option("mode") == "cc")echo 'selected="selected"'; ?> >Cc</option>
+                        <option value="cco" <?php if(get_option("mode") == "cco")echo 'selected="selected"'; ?> >Cco</option>
+                    </select>
+                </td>
+            </tr>
+            
+            <?php for($i = 0; $i < sizeof($nomes); $i++){?>
+                <tr valign="top">
+                    <th scope="row"><?=$nomes[$nomes_keys[$i]]['name']?></th>
+                    <td><input type="checkbox" name="<?=$nomes_keys[$i]?>" <?php if (get_option($nomes_keys[$i]) == "on") echo 'checked="checked"'; ?>/></td>
+                </tr>
+            <?php } ?>
 
         </table>        
         <?php submit_button(); ?>
@@ -80,23 +83,28 @@ function wpcf7_group_mail_settings_page() {
 
 add_filter( 'wpcf7_mail_components', 'wpcf7_group_mail_components', 10, 2 );
 
-function wpcf7_group_mail_components( $components, $wpcf7 ) {
-    $nomes = array("superadmin", "administrator", "editor", "author", "contributor", "subscriber");
-	 $values = '';
 
-    for($i = 0; $i <= 5; $i++){
-        if (get_option($nomes[$i]) == "on"){
-        $group_users = get_users('blog_id=1&orderby=nicename&role='.$nomes[$i]);
+function wpcf7_group_mail_components( $components, $wpcf7 ) {
+    
+    global $wp_roles;
+    $nomes = apply_filters('editable_roles', $wp_roles->roles);
+    $nomes_keys = array_keys($nomes);
+    $values = '';
+
+    for($i = 0; $i < sizeof($nomes_keys); $i++){
+        if (get_option($nomes_keys[$i]) == "on"){
+        $group_users = get_users('blog_id=1&orderby=nicename&role='.$nomes_keys[$i]);
         foreach ($group_users as $user) {
-        		$values .= ', ' . $user->user_email ;
+                $values .= ', ' . $user->user_email ;
         };
     }}
 
-	if( get_option("mode") == "cc" ){ $exploded = explode(',', $values, 2);$components['additional_headers'] .= "Cc: ".$exploded[1];}
-	else if(get_option("mode") == "cco"){ $exploded = explode(',', $values, 2);$components['additional_headers'] .= "Cco: ".$exploded[1]; }
-	else{ $components['recipient'] .= $values;}
+    if (get_option("mode") == "cc" ){ $exploded = explode(',', $values, 2);$components['additional_headers'] .= "Cc: ".$exploded[1];}
+    else if (get_option("mode") == "cco"){ $exploded = explode(',', $values, 2);$components['additional_headers'] .= "Cco: ".$exploded[1]; }
+    else{ $components['recipient'] .= $values;}
 
 
     return $components;
 }
+
 ?>
