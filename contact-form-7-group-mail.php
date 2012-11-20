cb3caa4ae0b82e815e6d37b2e609ef3e783f1ada
gutobenn <gutobenn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-20 18:26:37 +0000

    adding version 1.5.7
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-group-mail/trunk@627822 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contact-form-7-group-mail.php
+++ contact-form-7-group-mail.php
@@ -3,7 +3,7 @@
 Plugin Name: Contact Form 7 Group Mail
 Plugin URI: http://www.u3b.com.br/plugins/contact-form-7-group-mail
 Description: Send 'Contact Form 7' mails to all users of any group (admins, editors, authors, contributors, subscribers and custom roles).
-Version: 1.2
+Version: 1.1.7
 Author: Augusto Bennemann
 Author URI: http://www.u3b.com.br
 License: GPL2
