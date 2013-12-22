5545315bcebd51d48bf948ef123e8bb43a6d1485
gutobenn <gutobenn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-22 23:08:02 +0000

    updated readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-group-mail/tags/1.3@827168 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ contact-form-7-group-mail.php
@@ -0,0 +1,173 @@
+<?php
+/*
+Plugin Name: Contact Form 7 Group Mail
+Plugin URI: http://www.u3b.com.br/plugins/contact-form-7-group-mail
+Description: Send 'Contact Form 7' mails to all users of any group (admins, editors, authors, contributors, subscribers and custom roles).
+Version: 1.3
+Author: Augusto Bennemann
+Author URI: http://www.u3b.com.br
+License: GPL2
+*/
+
+/*  Copyright 2012 Augusto Bennemann (email: gutobenn at gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// i18n
+function wpcf7_group_mail_plugins_loaded() {
+	load_plugin_textdomain( 'contact-form-7-group-mail', false, 'contact-form-7-group-mail/languages' );
+}
+add_action( 'plugins_loaded', 'wpcf7_group_mail_plugins_loaded' );
+
+function get_c_roles() {
+    global $wp_roles;
+
+    $all_roles = $wp_roles->roles;
+    $editable_roles = apply_filters( 'editable_roles' , $all_roles );
+    
+    $roles = array();
+    
+    foreach ( $editable_roles as $slug => $role ) {
+    	$roles[$slug] = $role['name'];
+    }
+
+    return $roles;
+}    
+
+function wpcf7_group_mail_components( $components, $wpcf7 ) {
+    
+    $settings = get_post_meta( $wpcf7->id, 'wpcf7_group_mail', true );
+    $values = array();
+
+   	foreach( $settings['roles'] as $slug => $role ) {
+        $group_users = get_users( "orderby=nicename&role={$role}" );
+        
+        foreach ($group_users as $user) {
+        	$values[] = $user->user_email ;
+        };
+    }
+
+    if( $settings['mode'] == "cc" ) {
+    	$components['additional_headers'] .= 'Cc: ' . implode( ', ', $values); 
+    } elseif( $settings['mode'] == "cco") {
+    	$components['additional_headers'] .= 'Cco: ' . implode( ', ', $values); 
+    } else { 
+    	$components['recipient'] .= ', ' . implode( ', ', $values);
+    }
+
+    return $components;
+}
+add_filter( 'wpcf7_mail_components', 'wpcf7_group_mail_components', 10, 2 );
+
+function wpcf7_group_mail_add_box_metabox( $post_id ) {
+	
+	add_meta_box( 
+		'wpcf7_group_mail_metabox', 
+		'Group Mail', 
+		'wpcf7_group_mail_metabox',
+		null,
+		'additional_settings',
+		'low'
+    );
+}
+add_action( 'wpcf7_add_meta_boxes', 'wpcf7_group_mail_add_box_metabox' );
+
+function wpcf7_group_mail_metabox( $post, $metabox ) {
+
+            	$roles = get_c_roles();
+            	$settings = get_post_meta( $post->id, 'wpcf7_group_mail', true );
+            ?>
+            	<div style="margin-bottom:6px;">
+		<label for="wpcf7_group_mail_mode"><?php _e( 'Mode', 'contact-form-7-group-mail' ) ?></label>
+                    <select id="wpcf7_group_mail_mode" name="wpcf7_group_mail_mode" style="display:inline-block;">
+                        <option value="normal" <?php if( $settings['mode'] == "normal" ) echo 'selected="selected"'; ?>><?php _e( 'Normal', 'contact-form-7-group-mail' ) ?></option>
+                        <option value="cc" <?php if( $settings['mode'] == "cc" )echo 'selected="selected"'; ?>><?php _e( 'Cc', 'contact-form-7-group-mail' ) ?></option>
+                        <option value="cco" <?php if( $settings['mode'] == "cco" )echo 'selected="selected"'; ?>><?php _e( 'Bcc', 'contact-form-7-group-mail' ) ?></option>
+                    </select>
+		</div>
+            
+            <?php foreach( $roles as $slug => $name ) : ?>
+                <div style="margin-bottom:6px;">
+                    <input type="checkbox" style="margin-bottom:-1px; margin-right:4px;" name="wpcf7_group_mail_role_<?=$slug?>" id="wpcf7_group_mail_role_<?=$slug?>" 
+                    	<?php if ( !empty( $settings['roles'] ) ) if( in_array( $slug, $settings['roles'] ) ) echo 'checked="checked"'; ?>/>
+		    <label for="wpcf7_group_mail_role_<?=$slug?>"><?=translate_user_role( $name ) ?></label>
+                </div>
+            <?php endforeach;
+}
+
+/**
+ * 
+ * @param WPCF7_ContactForm $wpcf7 Object with the form informations. 
+ */
+function wpcf7_group_mail_save( $wpcf7 ) {
+	wpcf7_group_update_meta( $wpcf7->id );
+}
+add_action( 'wpcf7_after_save', 'wpcf7_group_mail_save' );
+
+/**
+ * 
+ * @param WPCF7_ContactForm $new The new form object.
+ * @param WPCF7_ContactForm $old The copied form object.
+ */
+function wpcf7_group_mail_copy( $new, $old ) {
+	wpcf7_group_update_meta( $new->id );
+}
+apply_filters( 'wpcf7_copy', 'wpcf7_group_mail_copy' );
+
+function wpcf7_group_update_meta( $wpcf7_id ) {
+
+	$settings = array();
+
+	$settings['mode'] = $_POST['wpcf7_group_mail_mode'];
+	
+	
+	
+	$settings['roles'] = array();
+	$roles = get_c_roles();
+	foreach( $roles as $slug => $role ) {
+	
+		if( isset( $_POST["wpcf7_group_mail_role_{$slug}"] ) && $_POST["wpcf7_group_mail_role_{$slug}"] == "on"  ) {
+			$settings['roles'][] = $slug;
+		}
+	}
+	
+	update_post_meta( $wpcf7_id, 'wpcf7_group_mail', $settings );
+}
+
+
+
+/* detect old config (1.0 or older) and automatically reconfigure after installation. */
+function wpcf7_group_mail_reconfigure(){
+	if( get_option("mode") ){
+	$roles = get_c_roles();
+	$settings = array();
+
+	$settings['mode'] = get_option("mode");
+
+	foreach ($roles as $slug => $role ){
+		if( get_option($slug) == "on" ) $settings['roles'][]=$slug;
+	}
+
+	$posts_array = get_posts( array('post_type' => 'wpcf7_contact_form') );
+	foreach( $posts_array as $post ){
+		update_post_meta( $post->ID, 'wpcf7_group_mail', $settings );
+	}
+
+	}
+
+	delete_option("mode");
+}add_action('init','wpcf7_group_mail_reconfigure',1);
+?>
---
+++ readme.txt
@@ -0,0 +1,95 @@
+=== Contact Form 7 Group Mail ===
+Contributors: gutobenn, edpittol
+Donate link: http://www.u3b.com.br/donate
+Tags: contact, form, 7, mass, email, group, admin, subscriber, author, editor, contributor, custom roles, individual
+Requires at least: 3.6
+Tested up to: 3.8
+Stable tag: 1.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows you to send Contact Form 7 messages to any group (admins, editors, authors, contributor, subscribers and custom roles). 
+
+== Description ==
+
+Contact Form 7 Group Mail allows you to send messages to multiple users, according to their groups. With this plugin you can choose who (admins, editors, authors, contributors, subscribers and custom roles) are going to receive the e-mails. 
+
+The list of recipients is automatically updated, so it's not needed to change the contact form every time a user is added or removed. 
+
+Important: it is an extension for Contact Form 7 plugin.
+
+== Installation ==
+
+1. Upload 'contact-form-7-group-mail' folder to the '/wp-content/plugins/' directory
+2. Create or edit a 'Contact Form 7' form.
+3. Go to 'Group Mail' and select groups to receive the e-mails.
+
+== Frequently Asked Questions ==
+
+= Is it possible to use different configurations for each form? =
+
+Yes! Individual configuration is available.
+
+= Can I send the messages to a group that I created? =
+
+Yes, it works with custom roles.
+
+= Does it send 'Cc' and 'Bcc' e-mails? =
+
+Yes. It's possible since version 0.9
+
+= What version of Contact Form 7 I need? =
+
+This plugin was succesfully tested on 3.3.1, but it probably works with other versions.
+
+== Screenshots ==
+
+1. Form Settings
+2. Multiple usage: sending emails to users.
+
+== Changelog ==
+
+= 1.3 =
+* Fixed bugs in WP 3.8 and CF7 3.6
+
+= 1.1.7 =
+* Added translation support.
+
+= 1.1.5 =
+* Individual configuration available
+
+= 1.1 =
+* Individual configuration available
+
+= 1.0 =
+* Works with custom roles
+
+= 0.9 =
+* Added "Cc" and "Cco" options
+
+= 0.8 =
+* Added 'contributors' option.
+* Lighter code.
+
+== Upgrade Notice ==
+
+= 1.1.7 =
+* Fixed bugs on WordPress 3.8 and Contact Form 7 3.6. Translation also.
+
+= 1.1.7 =
+* Plugin now allows translations. Portuguese available.
+
+= 1.1.5 =
+If you installed 1.1, please update! This version allows individual configuration. IMPORTANT: Global configuration isn't available anymore, so the active configuration will be set to all forms.
+
+= 1.1 =
+This version allows individual configuration. IMPORTANT: Global configuration isn't available anymore, so the active configuration will be set to all forms.
+
+= 1.0 =
+This version allows you to use custom roles.
+
+= 0.9 =
+This version adds "Cc" and "Cco" options to plugin.
+
+= 0.8 =
+This version adds a new 'contributors' option to plugin and is lighter.
